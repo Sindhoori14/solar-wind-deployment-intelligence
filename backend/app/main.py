6af082d47507008835a3b2703ba89ee6b4776f34
Sindhoori14 <pulisindhu54@gmail.com>
2026-07-09 17:52:57 +0530

    Implemented user authentication module

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.database import Base, engine
+
+from app.models.user import User
+from app.routers.auth import router as auth_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Solar & Wind Deployment Intelligence Platform")
+
+app.include_router(auth_router)
+
+
+@app.get("/")
+def home():
+    return {"message": "Backend is working successfully!"}
---
+++ backend/app/routers/auth.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.models.user import User
+from app.schemas.user import UserCreate, UserLogin
+from app.utils.security import hash_password
+from app.utils.security import verify_password
+from app.auth import create_access_token
+router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+
+@router.post("/signup")
+def signup(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = User(
+        email=user.email,
+        password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User created successfully"}
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if db_user is None:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    if not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    access_token = create_access_token(
+        data={"sub": db_user.email}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
